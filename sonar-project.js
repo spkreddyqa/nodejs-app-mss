2969d2a3a03316111a3f6e6481dc9b5b33f868f4
spkreddyqa <103243429+spkreddyqa@users.noreply.github.com>
2023-06-14 16:50:10 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.205.239.19:9000/',
+  serverUrl: 'http://13.232.84.184:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_d0cfacefac9fb8ab65a0479c41bf5900483b0354',
+	    'sonar.login': 'squ_0b7a075ed6cc21acab0a790c0a0701355aecae25',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
